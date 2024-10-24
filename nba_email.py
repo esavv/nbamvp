45154bad780ef2cc091f4a6e197d3b2ed6b497ac
Erik Savage <erikmsavage@gmail.com>
2024-10-24 12:59:59 -0400

    Additional preseason notify changes

--- nba_email.py
+++ nba_email.py
@@ -9,59 +9,15 @@ import pandas as pd
 # Issue from 10/28/2022: https://stackoverflow.com/questions/72478573/how-to-send-an-email-using-python-after-googles-policy-update-on-not-allowing-j
 #   Update: Fixed on 11/13/2022
 
-def send_preseason_email(year, today, season_start, season_end, weeks_til_start, predict_start_date):
-  print("\nEmail execution goes here!\n")
-  port = 465  # For SSL
-  smtp_server = "smtp.gmail.com"
-  sender_email = "eriksmvppredictions@gmail.com"
-  reader = csv.reader(open('pw.csv', 'r'))       # App Password for new email, generated on 11/14/2022
-  password = next(reader)[0]
-  subject = str(year) + " NBA MVP Predictions - Preseason"
-  # Generate the email list
-  email_list = 'test_emails.csv'
-  reader = csv.reader(open(email_list, 'r'))
-  bcc_emails = [row[0] for row in reader]
-
-  # Create a multipart message and set headers
-  message = MIMEMultipart() # MIMEMultipart("alternative")
-  message["From"] = "Erik's MVP Predictions <" + sender_email + ">"
-  message["Subject"] = subject
-
-  # Get the empty email body template 
-  with open('email_template_preseason.html', 'r') as template_file:
-    html_template = template_file.read()
-
-  # TODO: Populate the template with variables
-  #html = html_template.format(table_html=table_html)
-
-  # Add body to email
-  body = MIMEText(html_template, "html")
-  message.attach(body)
-
-  # Convert message to string
-  msg_body = message.as_string()
-
-  # Do some SSL stuff
-  context = ssl.create_default_context()
-  context.check_hostname = False
-  context.verify_mode = ssl.CERT_NONE
-
-  # Send the email
-  with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, bcc_emails, msg_body)
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "eriksmvppredictions@gmail.com"
+# sender_email = "eriksmvppredictor@gmail.com"  # The old email
+# reader = csv.reader(open('pw_old.csv', 'r'))  # App Password for old email, generated on 11/13/2022
+reader = csv.reader(open('pw.csv', 'r'))       # App Password for new email, generated on 11/14/2022
+password = next(reader)[0]
 
 def send_nba_email(prediction_file, year, week, is_prod_email, is_last_week):
-  port = 465  # For SSL
-  smtp_server = "smtp.gmail.com"
-  sender_email = "eriksmvppredictions@gmail.com"
-  # sender_email = "eriksmvppredictor@gmail.com"  # The old email
-  # receiver_email = "erikmsavage@gmail.com"      # Saving this for reference, but don't uncomment this.
-  
-  # reader = csv.reader(open('pw_old.csv', 'r'))  # App Password for old email, generated on 11/13/2022
-  reader = csv.reader(open('pw.csv', 'r'))       # App Password for new email, generated on 11/14/2022
-  password = next(reader)[0]
-
   subject = str(year) + " NBA MVP Predictions - Week " + str(week)
   if is_last_week:
     subject = str(year) + " NBA MVP Predictions - Final Week (Week " + str(week) + ")"
@@ -127,6 +83,46 @@ def send_nba_email(prediction_file, year, week, is_prod_email, is_last_week):
 
   # Add attachment to message and convert message to string
   message.attach(part)
+  msg_body = message.as_string()
+
+  # Do some SSL stuff
+  context = ssl.create_default_context()
+  context.check_hostname = False
+  context.verify_mode = ssl.CERT_NONE
+
+  # Send the email
+  with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, bcc_emails, msg_body)
+
+def send_preseason_email(year, season_start, season_end, weeks_til_start, predict_start_date):
+  print("\nSending a preseason email notification!\n")
+  if weeks_til_start == 1:
+    subject = str(year) + " NBA MVP Preseason: First Prediction in " + str(weeks_til_start) + " Week!"
+  else: 
+    subject = str(year) + " NBA MVP Preseason: First Prediction in " + str(weeks_til_start) + " Weeks!"
+  # Generate the email list
+  email_list = 'test_emails.csv'
+  reader = csv.reader(open(email_list, 'r'))
+  bcc_emails = [row[0] for row in reader]
+
+  # Create a multipart message and set headers
+  message = MIMEMultipart() # MIMEMultipart("alternative")
+  message["From"] = "Erik's MVP Predictions <" + sender_email + ">"
+  message["Subject"] = subject
+
+  # Get the empty email body template 
+  with open('email_template_preseason.html', 'r') as template_file:
+    html_template = template_file.read()
+
+  # Populate the template with variables
+  html = html_template.format(season_start=season_start, season_end=season_end, predict_start_date=predict_start_date)
+
+  # Add body to email
+  body = MIMEText(html, "html")
+  message.attach(body)
+
+  # Convert message to string
   msg_body = message.as_string()
 
   # Do some SSL stuff
