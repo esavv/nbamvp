ffc3de281ae7f2bcc0b6cb130d16d4a291e528eb
Erik Savage <erikmsavage@gmail.com>
2024-11-30 14:22:14 -0500

    More email refactoring

--- src/nba_email.py
+++ src/nba_email.py
@@ -15,22 +15,6 @@ password = next(reader)[0]
 message = MIMEMultipart() # MIMEMultipart("alternative")
 message["From"] = "Erik's MVP Predictions <" + sender_email + ">"
 
-def finalize_email(subject, email_list, message):
-  message["Subject"] = subject
-  msg_body = message.as_string()
-  reader = csv.reader(open(email_list, 'r'))
-  bcc_emails = [row[0] for row in reader]
-
-  # Do some SSL stuff
-  context = ssl.create_default_context()
-  context.check_hostname = False
-  context.verify_mode = ssl.CERT_NONE
-
-  # Send the email
-  with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, bcc_emails, msg_body)
-
 def send_nba_email(prediction_file, year, week, mode, is_last_week):
   subject = str(year) + " NBA MVP Predictions - Week " + str(week)
   if is_last_week:
@@ -43,55 +27,39 @@ def send_nba_email(prediction_file, year, week, mode, is_last_week):
     subject = "[TEST] " + subject
     email_list = '../data/email/test_emails.csv'
 
-  # Build the table for the email...
-  # Read csv using pandas and then convert the csv to html string
-  # ... this code later will add into body
+  # Read, trim, and format the predictions table for the email body
   df = pd.read_csv(prediction_file)
   df = df.head(n = 30)
   df['Rank'] = df['Rank'].astype(int)
   df['Predicted Votes'] = df['Predicted Votes'].astype(int)
-  
-  # Generate the table
   table_html = pretty_html_table.build_table(df, 'blue_light')
 
-  # Get the empty email body template 
+  # Populate the email template with the formatted predictions table
   email_template_path = '../static/html/email_template.html'
   with open(email_template_path, 'r') as template_file:
     html_template = template_file.read()
-
-  # Add the table to the template
   html = html_template.format(table_html=table_html)
 
-  # Save the table-populated hmtl for debugging & browser preview
+  # Save the email body hmtl for debugging & browser preview
   email_body_path = '../static/html/email_body.html'
   with open(email_body_path, 'w') as html_file:
     html_file.write(html)
 
-  # Add body to email
-  body = MIMEText(html, "html")
-  message.attach(body)
-
-  # Open csv file in binary mode
+  # Prepare the predictions csv file as an attachment
   with open(prediction_file, "rb") as attachment:
       # Add file as application/octet-stream
       # Email client can usually download this automatically as attachment
       part = MIMEBase("application", "octet-stream")
       part.set_payload(attachment.read())
-
-  # Encode file in ASCII characters to send by email    
   encoders.encode_base64(part)
-
-  # Add header as key/value pair to attachment part
   part.add_header(
       "Content-Disposition",
       f'attachment; filename="{os.path.basename(prediction_file)}"',
   )
-  message.attach(part)
 
-  finalize_email(subject, email_list, message)
+  finalize_email(email_list, subject, html, part)
 
 def send_preseason_email(year, season_start, season_end, weeks_til_start, predict_start_date):
-  print("\nSending a preseason email notification!\n")
   if weeks_til_start == 1:
     subject = str(year) + " NBA MVP Preseason: First Prediction in " + str(weeks_til_start) + " Week!"
   else: 
@@ -106,11 +74,7 @@ def send_preseason_email(year, season_start, season_end, weeks_til_start, predic
   # Populate the template with variables
   html = html_template.format(season_start=season_start, season_end=season_end, predict_start_date=predict_start_date)
 
-  # Add body to email
-  body = MIMEText(html, "html")
-  message.attach(body)
-
-  finalize_email(subject, email_list, message)
+  finalize_email(email_list, subject, html)
 
 def send_error_email(year, week, traceback_str):
   print("\nSending an error email notification!\n")
@@ -125,11 +89,27 @@ def send_error_email(year, week, traceback_str):
     html_template = template_file.read()
 
   # Populate the template with variables
-  # html = html_template.format(traceback_str=traceback_str)
   html = html_template.replace("{traceback_str}", traceback_str)
 
-  # Add body to email
+  finalize_email(email_list, subject, html)
+
+def finalize_email(email_list, subject, html, part=None):
+  reader = csv.reader(open(email_list, 'r'))
+  bcc_emails = [row[0] for row in reader]
+
+  message["Subject"] = subject
   body = MIMEText(html, "html")
   message.attach(body)
+  if part:
+    message.attach(part)
+  msg_body = message.as_string()
 
-  finalize_email(subject, email_list, message)
+  # Do some SSL stuff
+  context = ssl.create_default_context()
+  context.check_hostname = False
+  context.verify_mode = ssl.CERT_NONE
+
+  # Send the email
+  with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, bcc_emails, msg_body)
