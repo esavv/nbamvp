aa3797e2aca4e3264e2432603dc8570dbe282856
Erik Savage <erikmsavage@gmail.com>
2025-09-24 19:27:04 -0400

    chore: refactor email sender; consolidate enums

--- src/nba_email.py
+++ src/nba_email.py
@@ -15,17 +15,26 @@ password = next(reader)[0]
 message = MIMEMultipart() # MIMEMultipart("alternative")
 message["From"] = "Erik's MVP Predictions <" + sender_email + ">"
 
+prod_emails = '../data/email/prod_emails.csv'
+admin_emails = '../data/email/test_emails.csv'
+
+main_template_path = '../static/html/email_template.html'
+main_body_path = '../static/html/email_body.html'
+preseason_template_path = '../static/html/email_template_preseason.html'
+postseason_template_path = '../static/html/email_template_postseason.html'
+error_template_path = '../static/html/email_template_error.html'
+
 def send_nba_email(prediction_file, year, week, mode, is_last_week):
+  # Example subject: "2022 NBA MVP Predictions - Week 12"
   subject = str(year) + " NBA MVP Predictions - Week " + str(week)
   if is_last_week:
     subject = str(year) + " NBA MVP Predictions - Final Week (Week " + str(week) + ")"
     
   if mode == 'prod':     
-    # Example subject: "2022 NBA MVP Predictions - Week 12"
-    email_list = '../data/email/prod_emails.csv'
+    email_list = prod_emails
   else:
     subject = "[TEST] " + subject
-    email_list = '../data/email/test_emails.csv'
+    email_list = admin_emails
 
   # Read, trim, and format the predictions table for the email body
   df = pd.read_csv(prediction_file)
@@ -33,16 +42,14 @@ def send_nba_email(prediction_file, year, week, mode, is_last_week):
   df['Rank'] = df['Rank'].astype(int)
   df['Predicted Votes'] = df['Predicted Votes'].astype(int)
   table_html = pretty_html_table.build_table(df, 'blue_light')
-
+ 
   # Populate the email template with the formatted predictions table
-  email_template_path = '../static/html/email_template.html'
-  with open(email_template_path, 'r') as template_file:
+  with open(main_template_path, 'r') as template_file:
     html_template = template_file.read()
   html = html_template.format(table_html=table_html)
 
   # Save the email body hmtl for debugging & browser preview
-  email_body_path = '../static/html/email_body.html'
-  with open(email_body_path, 'w') as html_file:
+  with open(main_body_path, 'w') as html_file:
     html_file.write(html)
 
   # Prepare the predictions csv file as an attachment
@@ -64,48 +71,35 @@ def send_preseason_email(year, season_start, season_end, weeks_til_start, predic
     subject = str(year) + " NBA MVP Preseason: First Prediction in " + str(weeks_til_start) + " Week!"
   else: 
     subject = str(year) + " NBA MVP Preseason: First Prediction in " + str(weeks_til_start) + " Weeks!"
-  email_list = '../data/email/test_emails.csv'
 
-  # Get the empty email body template 
-  email_template_preseason_path = '../static/html/email_template_preseason.html'
-  with open(email_template_preseason_path, 'r') as template_file:
+  # Get the email template & populate with variables
+  with open(preseason_template_path, 'r') as template_file:
     html_template = template_file.read()
-
-  # Populate the template with variables
   html = html_template.format(season_start=season_start, season_end=season_end, predict_start_date=predict_start_date)
 
-  finalize_email(email_list, subject, html)
+  finalize_email(admin_emails, subject, html)
 
 def send_postseason_email( year, season_end ):
   subject = str(year) + " NBA MVP Postseason Notification"
-  email_list = '../data/email/test_emails.csv'
   
-  # Get the empty email body template 
-  email_template_postseason_path = '../static/html/email_template_postseason.html'
-  with open(email_template_postseason_path, 'r') as template_file:
+  # Get the email template & populate with variables
+  with open(postseason_template_path, 'r') as template_file:
     html_template = template_file.read()
-  
-  # Populate the template with variables
   html = html_template.format(season_end=season_end)
   
-  finalize_email(email_list, subject, html)
+  finalize_email(admin_emails, subject, html)
 
 def send_error_email(year, week, traceback_str):
   print("\nSending an error email notification!\n")
 
   subject = "ERROR: " + str(year) + " NBA MVP Predictions - Week " + str(week)
 
-  email_list = '../data/email/test_emails.csv'
-
-  # Get the empty email body template 
-  email_template_preseason_path = '../static/html/email_template_error.html'
-  with open(email_template_preseason_path, 'r') as template_file:
+  # Get the email template & populate with variables
+  with open(error_template_path, 'r') as template_file:
     html_template = template_file.read()
-
-  # Populate the template with variables
   html = html_template.replace("{traceback_str}", traceback_str)
 
-  finalize_email(email_list, subject, html)
+  finalize_email(admin_emails, subject, html)
 
 def finalize_email(email_list, subject, html, part=None):
   reader = csv.reader(open(email_list, 'r'))
